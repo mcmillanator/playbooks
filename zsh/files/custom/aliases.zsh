f2d72390593fcf0921e0ae1788695c520243b031
mcmillanator <stan@stanleymcmillan.com>
2021-02-08 01:22:41 -0500

    add custom aliases to zsh

---
+++ zsh/files/custom/aliases.zsh
@@ -0,0 +1,12 @@
+# Oh my zsh
+alias zshconfig="vim ~/.zshrc"
+alias omz="vim ~/.oh-my-zsh"
+alias omzconfig="vim ~/.oh-my-zsh"
+
+alias myip="curl http://ipecho.net/plain; echo"
+alias apps="cd ~/Documents/Projects"
+alias la='ls -a'
+alias ll='ls -lah'
+
+# ruby
+alias be='bundle exec $@'
