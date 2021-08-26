75db4a96d2901decb65f7e92a0727ccb34b90d4c
mcmillanator <stan@stanleymcmillan.com>
2021-08-26 10:41:48 -0400

    remove old files

--- playbooks/i3wm/files/.i3/scripts/lock.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# i3-msg 'workspace 11; exec i3-sensible-terminal -e vim `which rails`'
-# sleep 0.5
-# maim ~/Pictures/lock.png
-i3lock -i ~/Pictures/lock.png # -u -n; i3-msg 'focus parent kill' ; i3-msg 'workspace previous'
